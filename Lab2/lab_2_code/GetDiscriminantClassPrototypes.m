55f58f804dcff491795511611eb312f474bb643c
Matthew Gulbronson <mgulbronson@gmail.com>
2022-04-04 23:12:02 -0400

    Finish sequential discriminants

--- Lab2/lab_2_code/GetDiscriminantClassPrototypes.m
+++ Lab2/lab_2_code/GetDiscriminantClassPrototypes.m
@@ -1,8 +1,7 @@
-function [classifierPrototypes, misclassified] = GetDiscriminantClassPrototypes(n, dataA, dataB)
-    perfectlySeparatedClasses = blanks(n);
-    classifierPrototypes = nan(n,2,2);
-    misclassified = false(n,length(dataA),2);
-    for j = 1:n
+function [classifierPrototypes, classified] = GetDiscriminantClassPrototypes(numDiscriminants, dataA, dataB)
+    classifierPrototypes = nan(numDiscriminants,2,2);
+    misclassified = false(numDiscriminants,length(dataA),2);
+    for j = 1:numDiscriminants
         while 1
             prototypeA = dataA(randi(length(dataA)),:);
             prototypeB = dataB(randi(length(dataB)),:);
@@ -15,7 +14,6 @@ function [classifierPrototypes, misclassified] = GetDiscriminantClassPrototypes(
                         dataB(i,:) = [nan nan];
                     end
                     dataB(any(isnan(dataB),2),:) = [];
-                    perfectlySeparatedClasses(j) = 'a';
                     misclassified(j,misclassifiedBIndices,2) = true;
                 end
                 if isempty(misclassifiedBIndices)
@@ -23,12 +21,34 @@ function [classifierPrototypes, misclassified] = GetDiscriminantClassPrototypes(
                         dataA(i,:) = [nan nan];
                     end
                     dataA(any(isnan(dataA),2),:) = [];
-                    perfectlySeparatedClasses(j) = 'b';
-                    misclassified(j,misclassifiedAIndices,2) = true;
+                    misclassified(j,misclassifiedAIndices,1) = true;
                 end
                 classifierPrototypes(j,:,1) = prototypeA;
                 classifierPrototypes(j,:,2) = prototypeB;
                 break
+            end
+        end
+    end
+    classified = false(length(dataA),2);
+    for i = 1:length(misclassified)
+        pointA = misclassified(:,i,1);
+        pointB = misclassified(:,i,2);
+        for j = 1:numDiscriminants
+            perfectlyClassifiedA = not(sum(misclassified(j,:,1)));
+            perfectlyClassifiedB = not(sum(misclassified(j,:,2)));
+            if perfectlyClassifiedA
+                classified(:,1) = true;
+                if not(pointB(j))
+                    classified(i,2) = true;
+                    break
+                end
+            end
+            if perfectlyClassifiedB
+                classified(:,2) = true;
+                if not(pointA(j))
+                    classified(i,1) = true;
+                    break
+                end
             end
         end
     end
--- Lab2/lab_2_code/sequential_discriminants.m
+++ Lab2/lab_2_code/sequential_discriminants.m
@@ -2,7 +2,7 @@ close all;
 
 PATH = "..\lab_2_data\lab2_3.mat";
 data = importdata(PATH);
-TESTS_TO_RUN = [1];
+TESTS_TO_RUN = [2];
 % rng(0, 'twister');
 
 %%% Learn 3 sequential classifiers and plot %%%
@@ -23,10 +23,10 @@ if sum(TESTS_TO_RUN == 1)
         plot(prototypeB(1), prototypeB(2), 'x', 'Color', 'w');
         diff = prototypeA - prototypeB;
         boundarySlope = diff(1) / -diff(2);
-        mean = (prototypeA + prototypeB) / 2;
+        prototypeMean = (prototypeA + prototypeB) / 2;
         xlimits = xlim; ylimits = ylim;
-        y1 = mean(2) + boundarySlope * (xlimits(1) - mean(1));
-        y2 = mean(2) + boundarySlope * (xlimits(2) - mean(1));
+        y1 = prototypeMean(2) + boundarySlope * (xlimits(1) - prototypeMean(1));
+        y2 = prototypeMean(2) + boundarySlope * (xlimits(2) - prototypeMean(1));
         plot([xlimits(1) xlimits(2)], [y1 y2], '--', 'Color', 'w');
         xlim([xlimits(1) xlimits(2)]); ylim([ylimits(1) ylimits(2)]);
         legend('\color{white}Class A', '\color{white}Class B', '','','');
@@ -34,5 +34,40 @@ if sum(TESTS_TO_RUN == 1)
 end
 
 if sum(TESTS_TO_RUN == 2)
-    [classifierPrototypes, misclassified] = GetDiscriminantClassPrototypes(NUM_DISCRIMINANTS, data.a, data.b);
+    NUM_CLASSIFIERS = 20;
+    NUM_DISCRIMINANTS = [1 2 3 4 5];
+    errorRates = nan(NUM_CLASSIFIERS, length(NUM_DISCRIMINANTS));
+    for jIndex = 1:length(NUM_DISCRIMINANTS)
+        j = NUM_DISCRIMINANTS(jIndex);
+        for i = 1:NUM_CLASSIFIERS
+            [classifierPrototypes, classified] = GetDiscriminantClassPrototypes(j, data.a, data.b);
+            errorRates(i,jIndex) = sum(classified(:) == false) / numel(classified);
+        end
+    end
+
+    figure; hold on;
+    set(gca, 'Color', 'k');
+    plot(NUM_DISCRIMINANTS, min(errorRates), '_', 'Color', 'white', 'MarkerSize', 30);
+    plot(NUM_DISCRIMINANTS, mean(errorRates), 'x', 'Color', '#0AD3FF', 'MarkerSize', 15);
+    plot(NUM_DISCRIMINANTS, max(errorRates), '_', 'Color', 'white', 'MarkerSize', 30);
+    xticks(NUM_DISCRIMINANTS);
+    xlim([0.5 5.5]);
+    yticks(0:0.05:1);
+    ylimits = ylim; ylim([-0.03 ylimits(2)]);
+    set(gca, 'YGrid', 'on', 'XGrid', 'off', 'GridColor', 'white');
+    legend('\color{white}Min/Max Error Rate', '\color{white}Mean Error Rate','');
+    title('Extrema and Mean Error Rates of Learned Sequential Discriminants');
+    xlabel('Linear discriminants per classifier');
+    ylabel('Error rate [decimal]');
+
+    figure; hold on;
+    set(gca, 'Color', 'k');
+    plot(NUM_DISCRIMINANTS, std(errorRates), 'x', 'Color', '#0AD3FF', 'MarkerSize', 15);
+    xticks(NUM_DISCRIMINANTS);
+    xlim([0.5 5.5]);
+    yticks(0:0.005:0.25);
+    set(gca, 'YGrid', 'on', 'XGrid', 'off', 'GridColor', 'white');
+    title('Error Rate Standard Deviation of Learned Sequential Discriminants');
+    xlabel('Linear discriminants per classifier');
+    ylabel('Error rate standard deviation [decimal]');
 end
